9bf427d6133f3fa26a51104d356b13356e08b58e
Daniele <danielee.gamba@gmail.com>
2018-10-08 15:45:37 +0200

    messe note

--- Assets/Cues.cs
+++ Assets/Cues.cs
@@ -4,51 +4,68 @@ using UnityEngine;
 
 public class Cues : MonoBehaviour {
 
-    GameObject plane;
-
-    // Use this for initialization
-
+    public GameObject cue;
     public Renderer rend;
-    void Start () {
-        
-        plane = GameObject.CreatePrimitive(PrimitiveType.Quad);
-        
-        rend = plane.GetComponent<Renderer>();
+    public GameObject hitLine;
+
+    private float debug;
+    private float s; //startPos
+
+    void Start()
+    {
+        rend = cue.GetComponent<Renderer>();
         Shader shader = Shader.Find("Cues/QuadBody");
         rend.material.shader = shader;
-        rend.material.SetFloat("_InvisibleBelow", 0.0f);
-        rend.material.SetColor("_BaseColor",new Color(1.0f, 0.0f, 0.0f) );
-        rend.material.SetColor("_ResonanceColor", new Color(0.0f, 1.0f, 0.0f));
-        rend.material.SetColor("_FrostColor", new Color(0.0f, 0.0f, 1.0f));
 
-        // per sicurezza tutti i param del frost
-        rend.material.SetFloat("_FrostTailCap", 0.06f);
-        rend.material.SetFloat("_FrostTailSlope", 1.15f);
-        rend.material.SetFloat("_FrostTailSmoothness", 0.59f);
-        rend.material.SetFloat("_FrostStartTime", 1.0f);
-        rend.material.SetFloat("_FrostT1", 0.25f);
-        rend.material.SetFloat("_FrostT2", 0.26f);
-        rend.material.SetFloat("_FrostT3", 0.68f);
-        rend.material.SetFloat("_FrostTailHeight", 0.14f);
-        rend.material.SetFloat("_FrostTailHeightOffset", 0.31f);
-        rend.material.SetFloat("_FrostTailFalloff", 1.7f);
-
-
-
-    }   
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        float distance = 6.0f - Time.time * 3.0f;        
-        plane.transform.position = new Vector3(0.0f, 3.0f, 0.0f);
-
-        float shaderTime = -(distance - 6.0f) / 6.0f;
-        rend.material.SetFloat("_FrostLeadingInTime", Time.time / 5.0f);
-        rend.material.SetFloat("_FrostRiseTime", Time.time / 5.0f-1.0f);
+        s = GetTailPos(cue);
     }
-    void OnGUI ()
+
+    void Update()
     {
-        GUILayout.Label((Time.time / 3.0f).ToString());
+        float speed = 1.0f;
+
+        float p = s - Time.time * speed; //position at time t
+        SetTailPos(cue, p);
+        float e = GetTopPos(hitLine); //hitline
+
+
+        float frostTime = 1.0f - (p - e) / (s - e);
+        float riseTime = Mathf.Max(0.0f, frostTime - 1.0f)*4.0f;
+        frostTime = Mathf.Min(frostTime, 1.0f);
+        riseTime = Mathf.Min(riseTime, 1.0f);
+
+        rend.material.SetFloat("_FrostLeadingInTime", Mathf.Min(frostTime, 1.0f));
+        rend.material.SetFloat("_FrostRiseTime", riseTime);
+
+
+        float invisibleBelow = (GetTopPos(hitLine) - GetTailPos(cue)) / cue.transform.localScale.y;
+
+        rend.material.SetFloat("_InvisibleBelow", invisibleBelow);
     }
+
+
+    void Debug(float f)
+    {
+        debug = f;
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(debug.ToString());
+    }
+
+    float GetTailPos(GameObject g)
+    {
+        return g.transform.position.y - g.transform.localScale.y / 2.0f;
+    }
+    void SetTailPos(GameObject g, float newP)
+    {
+        Vector3 newPos = new Vector3(0.0f, newP + g.transform.localScale.y/2.0f, 0.0f);
+        cue.transform.position = newPos;
+    }
+    float GetTopPos(GameObject g)
+    {
+        return g.transform.position.y + g.transform.localScale.y / 2.0f;
+    }
+
 }
